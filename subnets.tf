9a49eebb05a2cf31197e5b8b56c76541c3104ad5
DevOps Think <devopsthink@gmail.com>
2020-12-20 10:56:52 +0530

    modifed subnet CIDR range

--- subnets.tf
+++ subnets.tf
@@ -1,7 +1,7 @@
 # Public subnet creation
 resource "aws_subnet" "pgsql_public_subnet" {
   vpc_id                  = aws_vpc.pgsql_vpc.id
-  cidr_block              = "10.10.0.0/28"
+  cidr_block              = "10.10.10.0/28"
   availability_zone       = "us-west-2a"
   map_public_ip_on_launch = true
   tags = {
